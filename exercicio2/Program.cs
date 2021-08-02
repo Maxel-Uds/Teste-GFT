2964c4bf5e9335409a17fb6c35e605d2d4c7bd74
Maxel Udson Alves da Conceição <maxellopes32@gmail.com>
2021-08-02 16:53:22 -0300

    Exercício 2

---
+++ exercicio2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace exercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Random num = new Random();
+            int randomNum = num.Next(0, 7);
+
+            Console.Write("Digite um número: ");
+            int n = int.Parse(Console.ReadLine());
+
+            while(!(n == randomNum)) {
+                Console.Write("Você errou! Tente de novo: ");
+                n = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Parabéns!! Você acertou!");
+        }
+    }
+}
